38b20c273dee2e3c923be0453600ecb8f9074633
HSHKZ <lucasolegniel@gmail.com>
2024-07-19 16:44:57 +0200

    Write a script that lists all files in the current directory.

---
+++ shell_navigation/3-list_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# order : Write a script that lists all files in the current directory.
+# consigne : Écrivez un script qui répertorie tous les fichiers du répertoire actuel.
+# commande : ls (list / lister) + option -l (long format) 
+
+ls -la
