0de6327b27ac1f3a2c3b28f6ac27d2b44123e68b
HSHKZ <lucasolegniel@gmail.com>
2024-07-19 16:51:12 +0200

    Write a script that creates a directory named my_folder and then navigates into it.

---
+++ shell_navigation/4-create_and_navigate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# order : Write a script that creates a directory named my_folder and then navigates into it.
+# consigne : Écrivez un script qui crée un répertoire nommé my_folder, puis y navigue.
+# commande : mkdir (make directory / creer un repertoire)
+
+mkdir my_folder
+
+# commande : cd (change directory / changer de repertoire)
+
+cd my_folder
