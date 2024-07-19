8fdb0db5eb4e5ecf35c6ea0dcf0b84b11b98105f
HSHKZ <lucasolegniel@gmail.com>
2024-07-19 16:36:31 +0200

    Write a script that creates a directory named my_directory.

---
+++ shell_navigation/1-create_directory.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# order : Write a script that creates a directory named my_directory.
+# consigne : Écrivez un script qui crée un répertoire nommé my_directory.
+# commande : mkdir (make directory / creer un repertoire)
+
+mkdir my_directory
