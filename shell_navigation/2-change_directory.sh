d416fad15b8fd6f7bdbebe6cc40daeb7ed296c35
HSHKZ <lucasolegniel@gmail.com>
2024-07-19 16:40:34 +0200

    Write a script that changes the current directory to my_directory.

---
+++ shell_navigation/2-change_directory.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# order : Write a script that changes the current directory to my_directory.
+# consigne : Écrivez un script qui change le répertoire actuel en my_directory.
+# commande : cd (change directory / changer de repertoire)
+
+cd my_directory
